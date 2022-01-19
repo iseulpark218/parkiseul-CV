5a1e273b8d3773f471264c78c04c3cc5a8ac9861
Iseul Park <lovelyangelapark@gmail.com>
2022-01-20 05:19:23 +0900

    update

--- pages/personal/personal.tsx
+++ pages/personal/personal.tsx
@@ -1,5 +1,20 @@
+import styles from "../../styles/Home.module.css";
+
 const Personal = () => {
-  return <div>개인프로젝트 작업 중</div>;
+  return (
+    <div
+      className={styles.personal} // style={{ margin: "100px 100px 100px 100px" }}
+    >
+      {" "}
+      <p style={{ color: "red" }}>작업중</p>입니다.
+      {/* <div>
+        <h1>(작업중) YouTube Data API를 이용한 website</h1>
+      </div>
+      <div>
+        <h1>(작업중) starbucks website</h1>{" "}
+      </div> */}
+    </div>
+  );
 };
 
 export default Personal;
