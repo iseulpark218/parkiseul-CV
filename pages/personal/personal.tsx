9e298d6c07cc21b8427e4683077e222d989383e1
Iseul Park <lovelyangelapark@gmail.com>
2022-01-20 05:31:18 +0900

    Clearance capacity

--- pages/personal/personal.tsx
+++ pages/personal/personal.tsx
@@ -2,17 +2,8 @@ import styles from "../../styles/Home.module.css";
 
 const Personal = () => {
   return (
-    <div
-      className={styles.personal} // style={{ margin: "100px 100px 100px 100px" }}
-    >
-      {" "}
+    <div className={styles.personal}>
       <p style={{ color: "red" }}>작업중</p>입니다.
-      {/* <div>
-        <h1>(작업중) YouTube Data API를 이용한 website</h1>
-      </div>
-      <div>
-        <h1>(작업중) starbucks website</h1>{" "}
-      </div> */}
     </div>
   );
 };
