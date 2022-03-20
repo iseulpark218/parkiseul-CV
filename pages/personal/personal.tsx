9bb2aefeb497d3a3d343db815ea73439fe51cc4a
Iseul Park <lovelyangelapark@gmail.com>
2022-03-21 01:10:58 +0900

    update

--- pages/personal/personal.tsx
+++ pages/personal/personal.tsx
@@ -2,13 +2,15 @@ import styles from "../../styles/personal.module.css";
 import img_covidMap from "../../public/covidMap.png";
 import img_webpub_1_1 from "../../public/webpub_1_1.png";
 import img_webpub_1_2 from "../../public/webpub_1_2.png";
+import img_webpub_2_1 from "../../public/webpub_2_1.png";
+import img_webpub_2_2 from "../../public/webpub_2_2.png";
 import img_marker from "../../public/covid_marker.png";
 
 const Personal = () => {
   return (
     <div className={styles.main}>
       <div className={styles.head}>
-        <a>개인프로젝트 (프론트엔드 위주)</a>
+        <div>개인프로젝트 (프론트엔드 위주)</div>
       </div>
       {/* 프로젝트 -1 */}
       <div>
@@ -45,9 +47,9 @@ const Personal = () => {
       <div>
         <div className={styles.title}>
           {" "}
-          <a href="./project3/home">Web Publishing</a>
+          <a href="./project3/home">Web Publishing-1</a>
         </div>
-        <p className={styles.content}>스타벅스 FW21 사이트</p>
+        <p className={styles.content}>[html+css+js] 스타벅스 FW21 사이트</p>
         <img className={styles.img} src={img_webpub_1_1.src} />
       </div>
       {/* 진행중 */}
@@ -58,6 +60,28 @@ const Personal = () => {
       <img
         style={{ width: "auto", height: "250px", paddingLeft: "30px" }}
         src={img_webpub_1_2.src}
+      />
+      {/* 프로젝트 -4 */}
+      <div>
+        <div className={styles.title}>
+          {" "}
+          <a href="https://git2022-webpublishing-1.vercel.app/">
+            Web Publishing-2
+          </a>
+        </div>
+        <p className={styles.content}>[html+css] 패션 매거진 반응형 사이트</p>
+        <img className={styles.img} src={img_webpub_2_1.src} />
+      </div>
+      {/* 진행중 */}
+      <div className={styles.subTitle}>현 진행상황</div>
+      <p className={styles.content}>- 서브메뉴 페이지 구상 중</p>
+      <img
+        style={{
+          width: "auto",
+          height: "250px",
+          paddingLeft: "30px",
+        }}
+        src={img_webpub_2_2.src}
       />
     </div>
   );
